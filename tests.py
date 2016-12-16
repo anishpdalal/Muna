a789597f1834aef115b745d6c33e772de64d8c55
Anish Dalal <anishdalal1@gmail.com>
2016-12-15 22:42:55 -0500

    adjusted tests based on refactoring

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
+import time
 from textblob import TextBlob
-import question_generator
+from text import Text
 
 TEXT = '''
 The titular threat of The Blob has always struck me as the ultimate movie
@@ -17,23 +18,23 @@ SENTENCE = TextBlob(TEXT).sentences[0]
 
 def run_tests():
     """ Need to refactor tests """
-    assert "\n" not in question_generator.clean_up_sentence(SENTENCE)
-    assert "--" in question_generator.clean_up_sentence(SENTENCE)
-    assert question_generator.generate_cloze_question(SENTENCE, "titular threat") == (
+
+    text = Text(TEXT, "Anish")
+    assert text.reader == "Anish"
+    assert text.text == TEXT
+    assert text.recorded_at == time.strftime("%m-%d-%Y %I:%M %p", time.localtime())
+    assert "\n" not in text._clean_sentence(SENTENCE)
+    assert "--" in text._clean_sentence(SENTENCE)
+    assert text._create_cloze_question(SENTENCE, "titular threat") == (
         'the ______________ of the blob has always struck me as the ultimate movie monster: an insatiably hungry, '
         'amoeba-like mass able to penetrate virtually any safeguard, capable of--as a doomed doctor chillingly '
         'describes it--"assimilating flesh on contact.',
         'titular threat')
-    assert question_generator.generate_cloze_questions(TEXT)[-1] == (
+    assert text.cloze_questions[-1] == (
         "snide comparisons to gelatin be damned, it's a concept with the most devastating of potential consequences, "
         "not unlike the grey goo scenario proposed by technological theorists fearful of"
         " ___________________________________.",
         'artificial intelligence run rampant')
-    cloze_questions = question_generator.generate_cloze_questions(TEXT)
-    assert len(cloze_questions) == 9
-    cloze_question, cloze_questions = question_generator.select_random_cloze_question(cloze_questions)
-    assert len(cloze_question) == 2
-    assert len(cloze_questions) == 8
 
     print("Tests pass!")
 
