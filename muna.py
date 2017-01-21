a3d62fb5a11874b304d9e1b7ce02c3153b68378d
Anish Dalal <anishdalal1@gmail.com>
2017-01-21 00:53:06 -0500

    random selection of cloze question is now in the executed python file.
    removed question_generator.py

--- muna.py
+++ muna.py
@@ -3,22 +3,20 @@ import random
 import time
 from text import Text
 
+TEXT = """J.J. Barea is expected to miss several games after straining his left calf for the third time this season.
+         "Same calf, different area," Mavs coach Rick Carlisle said. "Just unlucky. It's going to be a while.
+          It's going to be likely weeks and not days."
+       """
+
 if __name__ == "__main__":
-    user_input = input()
-    text = Text(user_input, "Anish")
+    text = Text(TEXT, "Anish")
     cloze_questions = text.cloze_questions
     while cloze_questions:
         print("Generating question...")
-        time.sleep(10)
         cloze_question = random.choice(cloze_questions)
-        time.sleep(10)
+        time.sleep(3)
         print(cloze_question[0])
-        input()
-        time.sleep(10)
+        time.sleep(5)
         print(cloze_question[1])
         cloze_questions.pop(cloze_questions.index(cloze_question))
     print("End of questioning!")
-
-
-
-
